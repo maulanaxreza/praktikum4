4d9e4728c15a0d0295c33170085d028aa8b5556d
unknown <17078.reza@gmail.com>
2022-09-21 08:04:54 +0700

    feature1

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('index',views.index,name='index'),
+    path('createdata',views.createdata,name='createdata'),
+    path('updatedata/<str:id>',views.updatedata,name='updatedata'),
+    path('deletedata/<str:id>',views.deletedata,name='deletedata')
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from . import models
 
 # Create your views here.
+def index(reqeust):
+    # Ambil data dari pelanggan
+    # All
+    allpelangganobj = models.pelanggan.objects.all()
+    # Get 
+    getpelangganobj = models.pelanggan.objects.get(idpelanggan=1)
+    # Filter
+    filterpelangganobj = models.pelanggan.objects.filter(jeniskelamin = 'Perempuan')
+
+    return render(reqeust,'pelanggan.html',{
+        "allpelangganobj" : allpelangganobj,
+        'getpelangganobj' : getpelangganobj,
+        'filterpelangganobj' : filterpelangganobj
+    })
+
+def createdata(request):
+    if request.method == "GET":
+        return render(request,'createdata.html')
+    else:
+        print(request.POST)
+        nama = request.POST['nama']
+        tanggal = request.POST['tanggal']
+        jeniskelamin = request.POST['jeniskelamin']
+        nohp = request.POST['nohp']
+
+        newpelanggan = models.pelanggan(
+            nama = nama,
+            tanggallahir = tanggal,
+            jeniskelamin = jeniskelamin,
+            nohp = nohp
+        ).save()
+        return redirect('index')
+
+def updatedata(request,id):
+    pelangganobj = models.pelanggan.objects.get(idpelanggan=id)
+    if request.method == 'GET':
+        return render(request,'updatedata.html',{
+            'pelangganobj':pelangganobj
+        })
+    else:
+        nama = request.POST['nama']
+        tanggal = request.POST['tanggal']
+        jeniskelamin = request.POST['jeniskelamin']
+        nohp = request.POST['nohp']
+        pelangganobj.nama = nama
+        pelangganobj.tanggal = tanggal
+        pelangganobj.jeniskelamin = jeniskelamin
+        pelangganobj.nohp = nohp
+        pelangganobj.save()
+
+        return redirect('index')
+
+def deletedata(request,id):
+    pelangganobj = models.pelanggan.objects.get(idpelanggan = id)
+    pelangganobj.delete()
+    return redirect('index')
